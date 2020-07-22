bac58c1907553a9e0d937c47c804be0002aa53a9
Ailea Patrinely <aileapatrinely@gmail.com>
2020-07-22 15:52:28 -0500

    Add to Cart button dispatches product to checkoutReducer

--- src/components/ProductListItem/ProductListItem.js
+++ src/components/ProductListItem/ProductListItem.js
@@ -4,16 +4,18 @@ import { connect } from 'react-redux';
 class ProductListItem extends Component {
   addProductToCart = () => {
     console.log(this.props.product);
-    // TODO: Dispatch here
+    this.props.dispatch({
+      type: 'ADD_TO_CART',
+      payload: this.props.product,
+    });
   };
 
   render() {
     return (
-      <div></div>
-      //   <li>
-      //     {this.props.product.name}: {this.props.product.price}{' '}
-      //     <button onClick={this.addProductToCart}>Add to Cart</button>
-      //   </li>
+      <li>
+        {this.props.product.name}: {this.props.product.price}{' '}
+        <button onClick={this.addProductToCart}>Add to Cart</button>
+      </li>
     );
   }
 }
